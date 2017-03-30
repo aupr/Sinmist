a77dc9777e5eb280d7b272dfc543cfa5cd97f2cb
Aman Ullah <Aman Ullah>
2017-03-30 16:46:39 +0600

    tested with plc

--- hptb/contents/plc/scripts/process.js
+++ hptb/contents/plc/scripts/process.js
@@ -6,6 +6,44 @@ var preprocess = function () {
     plcUserId = 'admin';
     plcPassword = "";
 
+    plcTag = {
+        vfd_start: '"Control".VFD_Control.Write_To_G120.Start',
+        vfd_stop: '"Control".VFD_Control.Write_To_G120.Stop',
+        vfd_frequency: '"Control".VFD_Control.Write_To_G120."Set Frequency"',
+        emfm_1_minLimit: '"Control".Flow_Meters.Electro_Magnetic."1"."Min Limit"',
+        emfm_1_maxLimit: '"Control".Flow_Meters.Electro_Magnetic."1"."Max Limit"',
+        emfm_2_minLimit: '"Control".Flow_Meters.Electro_Magnetic."2"."Min Limit"',
+        emfm_2_maxLimit: '"Control".Flow_Meters.Electro_Magnetic."2"."Max Limit"',
+        emfm_3_minLimit: '"Control".Flow_Meters.Electro_Magnetic."3"."Min Limit"',
+        emfm_3_maxLimit: '"Control".Flow_Meters.Electro_Magnetic."3"."Max Limit"',
+        emfm_4_minLimit: '"Control".Flow_Meters.Electro_Magnetic."4"."Min Limit"',
+        emfm_4_maxLimit: '"Control".Flow_Meters.Electro_Magnetic."4"."Max Limit"',
+        kgv_1_position: '"Control".Knife_Gate_Valve."1".Input',
+        kgv_2_position: '"Control".Knife_Gate_Valve."2".Input',
+        kgv_3_position: '"Control".Knife_Gate_Valve."3".Input',
+        kgv_4_position: '"Control".Knife_Gate_Valve."4".Input',
+        pt_1_minLimit: '"Control".PressureTransmitters."1"."Min Limit"',
+        pt_1_maxLimit: '"Control".PressureTransmitters."1"."Max Limit"',
+        pt_2_minLimit: '"Control".PressureTransmitters."2"."Min Limit"',
+        pt_2_maxLimit: '"Control".PressureTransmitters."2"."Max Limit"',
+        pt_3_minLimit: '"Control".PressureTransmitters."3"."Min Limit"',
+        pt_3_maxLimit: '"Control".PressureTransmitters."3"."Max Limit"',
+        pt_4_minLimit: '"Control".PressureTransmitters."4"."Min Limit"',
+        pt_4_maxLimit: '"Control".PressureTransmitters."4"."Max Limit"',
+        pt_5_minLimit: '"Control".PressureTransmitters."5"."Min Limit"',
+        pt_5_maxLimit: '"Control".PressureTransmitters."5"."Max Limit"',
+        pt_6_minLimit: '"Control".PressureTransmitters."6"."Min Limit"',
+        pt_6_maxLimit: '"Control".PressureTransmitters."6"."Max Limit"',
+        pt_7_minLimit: '"Control".PressureTransmitters."7"."Min Limit"',
+        pt_7_maxLimit: '"Control".PressureTransmitters."7"."Max Limit"',
+        pt_8_minLimit: '"Control".PressureTransmitters."8"."Min Limit"',
+        pt_8_maxLimit: '"Control".PressureTransmitters."8"."Max Limit"',
+        pt_9_minLimit: '"Control".PressureTransmitters."9"."Min Limit"',
+        pt_9_maxLimit: '"Control".PressureTransmitters."9"."Max Limit"',
+        pt_10_minLimit: '"Control".PressureTransmitters."10"."Min Limit"',
+        pt_10_maxLimit: '"Control".PressureTransmitters."10"."Max Limit"'
+    };
+
     //Auto login
     var spost = 'Login='+plcUserId+'&Password='+plcPassword;
     $.post("/FormLogin", spost, function(result) {
@@ -167,8 +205,8 @@ var takeDataHit = function () {
     });
     updateDataTable(dummyData);
     systemLog("Data hit pressed ... ","#system-log");
-    var startTag = '"Control".VFD_Control.Write_To_G120.Start=1';
-    $.get('data.html?'+startTag, function (result) {
+    var startTag = plcTag.pt_1_maxLimit+'=10';
+    $.post('limit.html',startTag, function (result) {
         console.log(result);
     }, 'json');
 
@@ -183,7 +221,7 @@ var emergencyHit = function () {
     });
     cbOptionsDisabled(false);
 
-    $.get('data.html?"Control".VFD_Control.Write_To_G120.Stop=1', function (result) {
+    $.get('data.html?'+plcTag.vfd_stop+'=1', function (result) {
         console.log(result);
     }, 'json');
 };
